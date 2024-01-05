b78993698101908ef71ff0e574b8071af73898b0
Hugo DEMENEZ <hdemenez@hotmail.fr>
2024-01-05 14:20:30 +0100

    bug: Auth component

--- components/header.tsx
+++ components/header.tsx
@@ -1,9 +1,15 @@
-'use client';
+import Authentication from "./authentication";
 import SteinProgramsLogo from "./logo/layout";
 import Notifications from "./notifications";
+import { Database } from '../types/supabase'
+import { createClientComponentClient, createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+import { cookies } from 'next/headers'
 
 
-export default function Header() {
+export default async function Header() {
+  const supabase = createServerComponentClient<Database>({ cookies })
+
+  const {data:user} = await supabase.auth.getUser()
     return (
         <nav className="w-full flex dark:bg-transparent justify-between sticky top-0 py-4 backdrop-blur-md  z-10 px-6">
             <div>
@@ -12,6 +18,7 @@ export default function Header() {
             
             <div className="flex-end">
                     <Notifications></Notifications>
+                    <Authentication user={user.user}></Authentication>
             </div>
         </nav>
     );
