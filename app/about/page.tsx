e4385ce782b392f8cdeed1a5f17411d9eca1abc4
Hugo DEMENEZ <hdemenez@hotmail.fr>
2024-01-06 17:39:50 +0100

    feat: Moved content to about and roadmap to home

---
+++ app/about/page.tsx
@@ -0,0 +1,55 @@
+
+
+export default function About(){
+    return(
+        <div className="py-6 lg:py-24 px-6">
+            <section id="how" className='dark:text-white'>
+                <h1 className='font-semibold text-3xl pt-8 pb-4 border-b-1 mb-4 border-gray-600'>How is it working ?</h1>
+                <div className=' leading-6 dark:text-gray-300 text-gray-900'>
+                    <ul>
+                        <li>
+                            <h1 className='font-semibold pt-6 text-xl'>Step 1: Comprehensive Data Gathering</h1>
+                            Our algorithm scours the vast landscape of the internet, meticulously gathering news and insights from diverse, reliable sources. We leave no stone unturned, ensuring you have access to the latest developments in the cryptocurrency world. 
+                        </li>
+                        <li>
+                            <h1 className='font-semibold pt-6 text-xl'>Step 2: Analyzing Market Influence</h1>
+                            Cryptocurrency markets are driven by an intricate web of factors, from news events to social media trends. Our algorithm goes beyond the surface, identifying the key influencers that impact market behavior. By harnessing the power of data analytics, we uncover the hidden forces at play.
+                        </li>
+                        <li>
+                            <h1 className='font-semibold pt-6 text-xl'>Step 3: Sentiment Analysis</h1>
+                            Not all news is created equal. Our algorithm employs a state-of-the-art, large language model, trained on extensive market data, to assess the sentiment of news articles and social media chatter. It can distinguish between positive and negative sentiments with unrivaled accuracy.
+                        </li>
+                        <li>
+                            <h1 className='font-semibold pt-6 text-xl'>Step 4: Empowering Your Decision-Making</h1>
+                            Knowledge is power, and with our algorithm, you gain access to real-time insights that can shape your cryptocurrency investment strategy. Whether you're a seasoned trader or a novice investor, our platform equips you with the tools to make informed decisions.
+                        </li>
+                    </ul>
+                </div>
+            </section>
+            <section id="why" className='dark:text-white'>
+                <h1 className='font-semibold text-3xl pt-8 pb-4 border-b-1 mb-4 border-gray-600'>Why Choose Us:</h1>
+                <div className='leading-6 dark:text-gray-300 text-gray-900'>
+                    <ul>
+                        <li>
+                            <h1 className='font-semibold pt-6 text-xl'>Accuracy: </h1>
+                            Our algorithm is fine-tuned to deliver precise and reliable information, ensuring you have the most accurate insights at your fingertips.
+                        </li>
+                        <li>
+                            <h1 className='font-semibold pt-6 text-xl'>Timeliness: </h1>
+                            Cryptocurrency markets move fast. Our real-time data updates keep you ahead of the curve, allowing you to seize opportunities as they arise.
+                        </li>
+                        <li>
+                            <h1 className='font-semibold pt-6 text-xl'>User-Friendly Interface:</h1>
+                            We've designed our platform with you in mind. It's intuitive and user-friendly, making it accessible to both experts and newcomers.
+                        </li>
+                        <li>
+                            <h1 className='font-semibold pt-6 text-xl'>Empowering Investors:</h1>
+                            Our mission is to empower you with the knowledge and tools needed to thrive in the world of cryptocurrency. Whether you're looking to capitalize on short-term trends or hold for the long term, we've got you covered.
+                        </li>
+                    </ul>
+                </div>
+            </section>
+        </div>
+    )
+}
+
--- app/page.tsx
+++ app/page.tsx
@@ -31,52 +31,40 @@ export default async function Home() {
                     </Link>
                 </div>
             </div>
-            <section id="how" className='dark:text-white'>
-                <h1 className='font-semibold text-3xl pt-8 pb-4 border-b-1 mb-4 border-gray-600'>How is it working ?</h1>
-                <div className=' leading-6 dark:text-gray-300 text-gray-900'>
-                    <ul>
-                        <li>
-                            <h1 className='font-semibold pt-6 text-xl'>Step 1: Comprehensive Data Gathering</h1>
-                            Our algorithm scours the vast landscape of the internet, meticulously gathering news and insights from diverse, reliable sources. We leave no stone unturned, ensuring you have access to the latest developments in the cryptocurrency world. 
-                        </li>
-                        <li>
-                            <h1 className='font-semibold pt-6 text-xl'>Step 2: Analyzing Market Influence</h1>
-                            Cryptocurrency markets are driven by an intricate web of factors, from news events to social media trends. Our algorithm goes beyond the surface, identifying the key influencers that impact market behavior. By harnessing the power of data analytics, we uncover the hidden forces at play.
-                        </li>
-                        <li>
-                            <h1 className='font-semibold pt-6 text-xl'>Step 3: Sentiment Analysis</h1>
-                            Not all news is created equal. Our algorithm employs a state-of-the-art, large language model, trained on extensive market data, to assess the sentiment of news articles and social media chatter. It can distinguish between positive and negative sentiments with unrivaled accuracy.
-                        </li>
-                        <li>
-                            <h1 className='font-semibold pt-6 text-xl'>Step 4: Empowering Your Decision-Making</h1>
-                            Knowledge is power, and with our algorithm, you gain access to real-time insights that can shape your cryptocurrency investment strategy. Whether you're a seasoned trader or a novice investor, our platform equips you with the tools to make informed decisions.
-                        </li>
-                    </ul>
-                </div>
-            </section>
-            <section id="why" className='dark:text-white'>
-                <h1 className='font-semibold text-3xl pt-8 pb-4 border-b-1 mb-4 border-gray-600'>Why Choose Us:</h1>
-                <div className='leading-6 dark:text-gray-300 text-gray-900'>
-                    <ul>
-                        <li>
-                            <h1 className='font-semibold pt-6 text-xl'>Accuracy: </h1>
-                            Our algorithm is fine-tuned to deliver precise and reliable information, ensuring you have the most accurate insights at your fingertips.
-                        </li>
-                        <li>
-                            <h1 className='font-semibold pt-6 text-xl'>Timeliness: </h1>
-                            Cryptocurrency markets move fast. Our real-time data updates keep you ahead of the curve, allowing you to seize opportunities as they arise.
-                        </li>
-                        <li>
-                            <h1 className='font-semibold pt-6 text-xl'>User-Friendly Interface:</h1>
-                            We've designed our platform with you in mind. It's intuitive and user-friendly, making it accessible to both experts and newcomers.
-                        </li>
-                        <li>
-                            <h1 className='font-semibold pt-6 text-xl'>Empowering Investors:</h1>
-                            Our mission is to empower you with the knowledge and tools needed to thrive in the world of cryptocurrency. Whether you're looking to capitalize on short-term trends or hold for the long term, we've got you covered.
-                        </li>
-                    </ul>
-                </div>
-            </section>
+            <div className="mx-auto max-w-5xl px-6 py-24 sm:py-32">
+                <ol className="relative border-l border-gray-200 dark:border-gray-700">                  
+                    <li className="mb-10 ml-4">
+                        <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+                        <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+                            September 2023
+                        </time>
+                        <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
+                            V0.0.0 public release
+                        </h3>
+                        <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
+                            Published the first working version of the web-app. Making Stein Programs&apos;produced news available to anyone.
+                        </p>
+                        <a href="#" className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-200 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700">
+                            Learn more 
+                            <svg className="w-3 h-3 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                            </svg>
+                        </a>
+                    </li>
+                    <li className="mb-10 ml-4">
+                        <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+                        <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">Decemeber 2023</time>
+                        <h3 className="text-lg font-semibold text-gray-900 dark:text-white">Annual report analyzer</h3>
+                        <p className="text-base font-normal text-gray-500 dark:text-gray-400">We plan to make our annual report algorithm analyzer available to the public by the end of 2023.</p>
+                    </li>
+                    <li className="ml-4">
+                        <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+                        <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">January 2024</time>
+                        <h3 className="text-lg font-semibold text-gray-900 dark:text-white">Trading Algorithm</h3>
+                        <p className="text-base font-normal text-gray-500 dark:text-gray-400">As we are developing all those news interpreter for automated trading purposes, we plan to publish our results in realtime on the website in early 2024.</p>
+                    </li>
+                </ol>
+            </div>
         </div>
     )
 }
