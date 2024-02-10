c4c47a61172fd314e1ea1cef9c39bac819358b3a
Hugo DEMENEZ <hdemenez@hotmail.fr>
2024-02-10 17:26:45 +0100

    bug: coherence between skeleton and content

--- app/search/[slug]/loading.tsx
+++ app/search/[slug]/loading.tsx
@@ -4,7 +4,7 @@ import { Skeleton } from "@/components/ui/skeleton"
 export default function Loading() {
     return (
         <div className="mx-auto max-w-5xl px-6 divide-y gap-8 mt-16">
-            <div className="h-full flex flex-col gap-4 py-16 sm:py-32 ">
+            <div className="h-full flex flex-col gap-4 py-4 ">
                 <Skeleton className="mt-2 h-12 w-full rounded-lg" />
                 <Skeleton className="h-4 w-32"/>
                 <Skeleton className="h-64 w-full" />
