bffc32ae1460bf24b5abadf4378e3c3d56c62a41
Hugo DEMENEZ <hdemenez@hotmail.fr>
2024-02-11 12:49:42 +0100

    feat: searchbar design and footer radius

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,8 +3,9 @@ import Link from "next/link"
 export default function Footer(){
     return (
         <footer className="w-full flex justify-between sticky bottom-0 py-4   z-10 px-6">
-            <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between  rounded-none shadow dark:shadow-white/30 bg-background/30 backdrop-blur-md">
-            <span className="text-sm text-gray-500 sm:text-center dark:text-gray-200">© 2023 <Link href="/" className="hover:underline">SteinPrograms</Link>. All Rights Reserved.
+            <div className="rounded-md w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between shadow dark:shadow-white/30 bg-background/30 backdrop-blur-md">
+            <span className="text-sm text-gray-500 sm:text-center dark:text-gray-200">
+                © 2023 <Link href="/" className="hover:underline">SteinPrograms</Link>. All Rights Reserved.
             </span>
             <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-200 sm:mt-0">
                 <li>
