1470ca8b8141f98ae31f30c35a749524dd96dc02
Hugo DEMENEZ <hdemenez@hotmail.fr>
2024-02-11 10:28:39 +0100

    bug: default request in backend

--- components/server/api.tsx
+++ components/server/api.tsx
@@ -6,10 +6,16 @@ export async function getLatestNews(symbol:string){
     //     resolve('resolved');
     //   }, 2000); // Delay for 2 seconds
     // });
-    const result = await fetch(`http://api.steinprograms.com:5050/news?symbol=${symbol}&MAX_PAGES=1&MAX_ARTICLE=3`,{
-        // Revalidate the data every 10 minutes
-        next: { revalidate: 600 },
-      })
-    const data = await result.json()
-    return data
+    try{
+
+        const result = await fetch(`http://api.steinprograms.com:5050/news?symbol=${symbol}`,{
+            // Revalidate the data every 10 minutes
+            next: { revalidate: 600 },
+        })
+        const data = await result.json()
+        return data
+    }
+    catch(e){
+        return []
+    }
 }
